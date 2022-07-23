bf5f2f3a67f5c7cdb75d54ef668bf5527279fd4c
Lin Tang <1508014225@qq.com>
2022-07-23 20:18:27 +0800

    完成算法题

--- src/SFT_Nummer.java
+++ src/SFT_Nummer.java
@@ -1,17 +1,42 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SFT_Nummer {
 //自然数的拆分问题
-public void sft() {
+int num,count = 0, sum = 0;
+    ArrayList<Integer> Nlist = new ArrayList<>();
+    public void sft() {
     Scanner s = new Scanner(System.in);
     System.out.println("请输入需要拆分的数字");
-    int num=s.nextInt();
-
-    int count = 0, sum = 0;
-
-
+    num=s.nextInt();
+    cf(num,1);
 }
 
 
-
+private void cf(int now,int st){
+    if(now>0) {
+        for(int i=st;i<num;i++) {
+            Nlist.add(i);
+            cf(now-i,i);
+            Nlist.remove(Nlist.size()-1);
+        }
+    }
+    else if(now==0) {
+        print();
+    }
 }
+
+
+//用于打印list
+ private void print(){
+     for(int i=0;i<Nlist.size();i++) {
+         System.out.print(Nlist.get(i));
+         if(i!=Nlist.size()-1) {
+             System.out.print("+");
+         }
+     }
+    System.out.println("");
+  }
+}
+
+
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,9 @@
 public class Test {
     public static void main(String[] args) {
       Counter c=new Counter();
+      SFT_Nummer s=new SFT_Nummer();
+      s.sft();
+
 
 
     }
