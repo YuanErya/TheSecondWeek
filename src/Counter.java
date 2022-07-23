72964f4b6a4f8b0a187c9298e982ed6fd40da8c1
Lin Tang <1508014225@qq.com>
2022-07-23 17:38:46 +0800

    计算器还未完成，先做算法题

--- src/Counter.java
+++ src/Counter.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 
 public class Counter  {
 //实现计算器的类
+    private  String st;
 
-   private String getsz(){
-       String st=new Scanner(System.in).nextLine();
+
+//获取用户输入的表达式,并检测输入的表达式的正确性
+   private ArrayList<Character> getsz(){
+       System.out.println("请输入所需要计算的表达式");
+       st=new Scanner(System.in).nextLine();
+       ArrayList<Character> fh=new ArrayList<Character>();
        boolean PD=true;//用于标记当前式子是否符合标准
        try{
        for (int i = 0; i <st.length(); i++) {
-           if(st.charAt(i)=='('||st.charAt(i)==')'||st.charAt(i)=='.'|| st.charAt(i)=='+'|| st.charAt(i)=='-'||st.charAt(i)=='*'||st.charAt(i)=='/'){
+           if(st.charAt(i)==40||st.charAt(i)==41||st.charAt(i)=='.'|| st.charAt(i)=='+'|| st.charAt(i)=='-'||st.charAt(i)=='*'||st.charAt(i)=='/'){
                //检测式子中的全部符号
-
+               fh.add(st.charAt(i));
            }else if(st.charAt(i)<=57&&st.charAt(i)>=48){
                //检测式子中的全部数字
            }else if(!((st.charAt(st.length()-1)<=57&&st.charAt(st.length()-1)>=48)||st.charAt(i)==')')){
@@ -28,13 +33,38 @@ public class Counter  {
        }
 
        if(PD){
-       return st;
+       return fh;
        }else{
            return null;
        }
    }//获取用户输入的表达式,并检测输入的表达式的正确性
 
+    //判断是否为符号位
+    private boolean pdd(int i){
+       if( st.charAt(i)=='+'|| st.charAt(i)=='-'||st.charAt(i)=='*'||st.charAt(i)=='/'){
+           return true;
+       }else{
+           return false;
+       }
+    }
+    //判断是否为符号位
 
+
+    //运算处理
+    private void ys(){
+       ArrayList<Character> fh=getsz();
+       if(fh!=null){
+           if(fh.indexOf('(')==-1){
+               for (int i = 0; i <st.length() ; i++) {
+                   if(fh.indexOf('*')==-1){}
+               }
+
+           }
+
+
+
+       }
+    }
 
 
 
---
+++ src/SFT_Nummer.java
@@ -0,0 +1,5 @@
+public class SFT_Nummer {
+
+
+
+}
