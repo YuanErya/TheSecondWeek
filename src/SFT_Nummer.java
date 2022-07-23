b7c600cb2a08f39b7971c5d4a7bdcf27f63eb117
Lin Tang <1508014225@qq.com>
2022-07-23 17:46:37 +0800

    计算器还未完成，先做算法题

--- src/SFT_Nummer.java
+++ src/SFT_Nummer.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class SFT_Nummer {
+//自然数的拆分问题
+public void sft() {
+    Scanner s = new Scanner(System.in);
+    System.out.println("请输入需要拆分的数字");
+    int num=s.nextInt();
+
+    int count = 0, sum = 0;
+
+
+}
 
 
 
