c6fbdc9b7bd7cdfe8a954bca3853b55868c82a75
Lin Tang <1508014225@qq.com>
2022-07-23 21:23:42 +0800

    双向链表完成增添和遍历功能

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -1,25 +1,53 @@
-public class DoubleLinkedList {
-//实现双向链表的类
+public class DoubleLinkedList<T> {
+    //实现双向链表的类
+    public DoubleLinkedList() {
 
+    }
 
+    //初始化链表
+    GT<T> now = new GT<>();
+    GT<T> begin = now;//记录首位
 
+    //往后增添的功能
+    public void add(T t) {
+        now.date = t;
+        GT<T> temp = now;//储存当前
+        now.next = new GT<T>(t);
+        now = now.next;
+        now.last = temp;
+    }
 
-
-
-
-
-
-
-
+    //输出遍历当前链表
+    public void print() {
+        System.out.print("[");
+        GT<T> d = begin;
+        while (true) {
+            if (d.next != null) {
+                System.out.print(d.date);
+                if(d.next.next!=null)
+                System.out.print(",");
+            }
+            if (d.next == null) {
+                break;
+            }
+            d=d.next;
+        }
+        System.out.print("]");
+    }
 
 }
 
-//单个节点
-class GT <T>{
+//泛型类的单个节点
+class GT<T> {
     T date;//储存的数据
-    GT last;//用于记录上一个节点；
-    GT next;//用于记录下一个节点
-    public GT(T t){
+    GT<T> last;//用于记录上一个节点；
+    GT<T> next;//用于记录下一个节点
+
+    public GT() {
+
+    }//无参构造，用于初始化；
+
+    public GT(T t) {
         this.date = t;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,14 @@ public class Test {
     public static void main(String[] args) {
         Counter c = new Counter();//计算器对象
         SFT_Nummer s = new SFT_Nummer();//算法题对象自然数的拆分//
-        s.sft();
+       // s.sft();
+
+        DoubleLinkedList<String> l = new DoubleLinkedList<String>();
+        l.add("I");
+        l.add("love");
+        l.add("you");
+        l.print();
+
 
 
     }
